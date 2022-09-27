92c522bc1545efaf54acbd847411d66d1d1d81aa
shariarpapon <cocclashin@gmail.com>
2022-09-27 02:18:16 -0400

    changes to panel drag logic

--- ContainerPanelDragger.cs
+++ ContainerPanelDragger.cs
@@ -6,18 +6,24 @@ namespace XEntity
     {
         private bool isDragging = false;
         private Vector2 offset;
+        private Transform mainPanel;
+
+        private void Awake() 
+        {
+            mainPanel = transform.parent;
+        }
 
         public void OnBeginDrag(PointerEventData eventData)
         {
-            offset = new Vector2(transform.localPosition.x, transform.localPosition.y) - new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-
+            offset = new Vector2(mainPanel.localPosition.x, mainPanel.localPosition.y) - new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            mainPanel.parent.SetAsLastSibling();
             isDragging = true;
         }
 
         public void OnDrag(PointerEventData eventData)
         {
             if (isDragging == false) return;
-            transform.localPosition = Input.mousePosition + new Vector3(offset.x, offset.y, 0);
+            mainPanel.localPosition = Input.mousePosition + new Vector3(offset.x, offset.y, 0);
         }
 
         public void OnEndDrag(PointerEventData eventData)
